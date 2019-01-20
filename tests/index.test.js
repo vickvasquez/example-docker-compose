9399f0f14d07a8566327a5b78a9c1f6d1bc9be19
Vick Vasquez <vasjvic@hotmail.com>
2019-01-19 21:51:39 -0600

    Addin simple test cases

---
+++ tests/index.test.js
@@ -0,0 +1,9 @@
+describe('Example', () => {
+	it('should return 3', () => {
+		expect(1+2).toBe(3)
+	})
+
+	it('should return success if response is greater than 0', () => {
+		expect(1 + 2).toBeGreaterThan(0)
+	})
+})
